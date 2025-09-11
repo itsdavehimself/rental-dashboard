28e68fb28825cf157d354bfbe2bbabbf050ede98
davehimself <itsdavehimself@users.noreply.github.com>
2025-09-10 20:59:31 -0500

    Add edit buttons to client info

--- client/src/containers/Events/CreateEvent/components/ResidentialClientInfo.tsx
+++ client/src/containers/Events/CreateEvent/components/ResidentialClientInfo.tsx
@@ -1,4 +1,4 @@
-import { Mail, PartyPopper, Phone, UserRound } from "lucide-react";
+import { Mail, PartyPopper, PenSquare, Phone, UserRound } from "lucide-react";
 import { formatPhoneNumber } from "../../../../helpers/formatPhoneNumber";
 import type { ClientDetail } from "../../../../types/Client";
 import ResidentialClientSection from "./ResidentialClientSection";
@@ -22,19 +22,34 @@ const ResidentialClientInfo: React.FC<ResidentialClientInfoProps> = ({
         <div className="flex flex-col border-1 border-gray-200 rounded-lg p-4">
           <h4 className="font-semibold text-lg">Client</h4>
           <div className="flex flex-col gap-2 mt-4 mb-4 text-sm">
-            <div className="flex gap-2 items-center">
-              <UserRound className="h-4 w-4 text-gray-400" />
-              <p>
-                {client.firstName} {client.lastName}
-              </p>
+            <div className="flex items-center justify-between">
+              <div className="flex gap-2 items-center">
+                <UserRound className="h-4 w-4 text-gray-400" />
+                <p>
+                  {client.firstName} {client.lastName}
+                </p>
+              </div>
+              <button className="font-semibold text-xs text-gray-500 hover:text-primary hover:cursor-pointer transition duration-200">
+                Change
+              </button>
             </div>
-            <div className="flex gap-2 items-center">
-              <Mail className="h-4 w-4 text-gray-400" />
-              <p>{client.email}</p>
+            <div className="flex items-center justify-between">
+              <div className="flex gap-2 items-center">
+                <Mail className="h-4 w-4 text-gray-400" />
+                <p>{client.email}</p>
+              </div>
+              <button className="flex justify-center items-center text-gray-500 hover:text-primary hover:cursor-pointer transition-all duration-200">
+                <PenSquare className="w-4 h-4" />
+              </button>
             </div>
-            <div className="flex gap-2 items-center">
-              <Phone className="h-4 w-4 text-gray-400" />
-              <p>{formatPhoneNumber(client.phoneNumber)}</p>
+            <div className="flex items-center justify-between">
+              <div className="flex gap-2 items-center">
+                <Phone className="h-4 w-4 text-gray-400" />
+                <p>{formatPhoneNumber(client.phoneNumber)}</p>
+              </div>
+              <button className="flex justify-center items-center text-gray-500 hover:text-primary hover:cursor-pointer transition-all duration-200">
+                <PenSquare className="w-4 h-4" />
+              </button>
             </div>
           </div>
           <hr className="text-gray-200" />
--- client/src/containers/Events/CreateEvent/components/ResidentialClientSection.tsx
+++ client/src/containers/Events/CreateEvent/components/ResidentialClientSection.tsx
@@ -1,3 +1,5 @@
+import { PenSquare } from "lucide-react";
+
 interface ResidentialClientSectionProps {
   title: string;
   children: React.ReactNode;
@@ -11,7 +13,13 @@ const ResidentialClientSection: React.FC<ResidentialClientSectionProps> = ({
 }) => {
   return (
     <>
-      <h5 className="font-semibold mt-6">{title}</h5>
+      <div className="flex justify-between items-end">
+        <h5 className="font-semibold mt-6">{title}</h5>
+        <button className="flex justify-center items-center text-gray-500 hover:text-primary hover:cursor-pointer transition-all duration-200">
+          <PenSquare className="w-4 h-4" />
+        </button>
+      </div>
+
       <div className={`flex flex-col text-sm mt-4 ${!lastItem && "mb-6"}`}>
         {children}
       </div>
